b089e117d78a17314cf04a3021fe43bd4c2872de
UWAJENEZANadine <adelineu640@gmail.com>
2022-04-22 14:29:34 +0200

    footer

--- src/components/order.js
+++ src/components/order.js
@@ -1,20 +1,23 @@
 import React from "react";
 import HomeLayout from "../components/homeLayout";
 import "./order.css";
-const Submit=()=>
-{
-    return(
-        <HomeLayout>
-           <div className="submit">
-                <div className="submit1">
-            <h1>Thanks!!!<br/><br/><br/>
-            You will receive an SMS with delivery information.<br/><br/>
-                </h1>
-                </div>
-                </div>
-         
-        </HomeLayout>
-    )
-
-}
+const Submit = () => {
+  return (
+    <HomeLayout>
+      <div className="submit">
+        <div className="submit1">
+          <h1>
+            Thanks!!!
+            <br />
+            <br />
+            <br />
+            You will receive an SMS with delivery information.
+            <br />
+            <br />
+          </h1>
+        </div>
+      </div>
+    </HomeLayout>
+  );
+};
 export default Submit;
